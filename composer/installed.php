a823169fdfef8573f13681beb22391af8468e37e
reeshmx <137994570+reeshmx@users.noreply.github.com>
2026-01-15 05:58:57 +0530

    Add files via upload

---
+++ composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-Main',
+        'version' => 'dev-Main',
+        'reference' => 'fadd2a1421062ac68fde3f15a39f45d7b79bcb54',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-Main',
+            'version' => 'dev-Main',
+            'reference' => 'fadd2a1421062ac68fde3f15a39f45d7b79bcb54',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v16.2.0',
+            'version' => '16.2.0.0',
+            'reference' => '813ae4961755af28a13bda451689f7a6ed6498cb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
